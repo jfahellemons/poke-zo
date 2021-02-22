a544280436541c4cd18f3ef80435320f645e90ab
Joep Hellemons <joep.hellemons@incentro.com>
2021-02-22 20:48:19 +0100

    changes

--- poke/src/app/components/organisms/quote/quote.component.ts
+++ poke/src/app/components/organisms/quote/quote.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.scss']
 })
 export class QuoteComponent implements OnInit {
+  @Input() variant: 'small' | 'large';
+  @Input() title: string;
+  @Input() description: string;
+  @Input() author: string;
 
   constructor() { }
 
